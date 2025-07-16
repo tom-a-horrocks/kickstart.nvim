62b5c5209b5774f5e5472ff42819aca181a3715b
Tom Horrocks <tom.a.horrocks@gmail.com>
2025-07-16 10:09:49 +0800

    Add git fugitive

--- init.lua
+++ init.lua
@@ -284,6 +284,10 @@ require('lazy').setup({
     },
   },
 
+  {
+    'tpope/vim-fugitive',
+  },
+
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
