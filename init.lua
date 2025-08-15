5debda74abc711f9bb7dd76c44825b2ca3b25474
Tom Horrocks <tom.a.horrocks@gmail.com>
2025-08-15 11:02:13 +0800

    Remove pyright... very distracting

--- init.lua
+++ init.lua
@@ -707,7 +707,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        basedpyright = {},
+        -- basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
