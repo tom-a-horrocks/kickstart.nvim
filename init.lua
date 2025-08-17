138c555f03d753f74b03e46afa135c308e3f3e27
Tom Horrocks <tom.a.horrocks@gmail.com>
2025-08-17 21:47:43 +0800

    Add basedpyright

--- init.lua
+++ init.lua
@@ -708,7 +708,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- Instructions for disabling ruff linting: https://github.com/astral-sh/ruff/issues/12795
-        -- basedpyright = {},
+        basedpyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
