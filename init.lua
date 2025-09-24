19b548b3424d99752af79bd51112aa4151aba1d3
Tom Horrocks <tom.a.horrocks@gmail.com>
2025-09-24 11:26:31 +0800

    Try nofsync for windows

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
+-- nofsync for windows
+if vim.fn.has 'win32' == 1 then
+  vim.g.nofsync = true
+end
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
